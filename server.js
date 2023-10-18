780efb270ae42ee98a68e179fe9b19cd8e03fece
Dev. Alex_dgreat <105602085+devalexdgreat@users.noreply.github.com>
2023-10-18 17:39:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,1 @@
-const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log(req.body.email);
-});
-
-server.listen(3000, 'localhost', () => {
-    console.log("Listening for requests on port 3000");
-});
